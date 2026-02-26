f51d5f58630cd14eabf7f55700023e2ed55bb00b
nickbraithwaite44 <nick.braithwaite@hotmail.co.uk>
2026-02-26 11:07:30 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,6 +32,7 @@ const Home = () => {
           <img src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4" />
         </a>
         <p className="mt-5">Welcome to 44scout</p>
+            <p>Make yourself known worldwide</p>
       </header>
     </div>
   );
