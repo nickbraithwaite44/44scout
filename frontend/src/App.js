ba13453245beee17c1e06d44c38bfeec6c904e65
nickbraithwaite44 <nick.braithwaite@hotmail.co.uk>
2026-02-26 10:10:31 +0100

    Update welcome message in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ const Home = () => {
         >
           <img src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4" />
         </a>
-        <p className="mt-5">Building something incredible ~!</p>
+        <p className="mt-5">Welcome to 44scout</p>
       </header>
     </div>
   );
